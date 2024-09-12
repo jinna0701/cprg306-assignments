83604856ebb097c950717a95e320a28416f31ba6
jin <jinna0701@gmail.com>
2024-09-11 20:32:03 -0600

    week2
    week2

--- app/week-2/StudnetInfo.js
+++ app/week-2/StudnetInfo.js
@@ -5,8 +5,9 @@ export default function StudentInfo() {
     return (
         <div>
             <h1>Hello, this is Jin 000480092</h1>
-            <Link href="https://github.com/jinna0701/cprg306-assignments">
+            <Link href="https://github.com/jinna0701?tab=repositories">
                 <p> click here to git repository link</p>
+                <p> thanks for the great lecture </p>
             </Link>
         </div>
     );
